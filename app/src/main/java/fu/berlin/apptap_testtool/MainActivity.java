39cdf637fa090b9107fdfffd182a23b866cc966a
s-interface <25102829+s-interface@users.noreply.github.com>
2020-01-23 12:46:04 +0100

    Initial commit

---
+++ app/src/main/java/fu/berlin/apptap_testtool/MainActivity.java
@@ -0,0 +1,14 @@
+package fu.berlin.apptap_testtool;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
